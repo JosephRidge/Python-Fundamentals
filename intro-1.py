536306c6b06ddb53f6abd1ca7f30c9141941427f
JosephRidge <josephridge18@gmail.com>
2025-11-13 08:57:50 +0300

    update

--- intro-1.py
+++ intro-1.py
@@ -16,3 +16,60 @@ DATA STRUCTURES
 BUILD: Webserver
 
 """
+
+
+"""
+Operators & Operands: 
+
+- Arithmetic operators: +,- ,/, //, %, *, **
+- Comparison Operators(acts as a predicate): ==, !=, <, >,<=, >=  
+- Boolean operators(Truth table): and, or, not 
+- is: identity operator
+- in: membership operator
+- bitwise(binary data/ information): &,|, ^, ~, <<, >> **
+
+
+NOTE: 
+when reassiigning a value of a variable we call that process variable overriding
+"""
+ 
+
+# Arithmetic operators: +,- ,/, //, %, *, **
+
+num1 = 10
+num2 = 20
+
+num1 = 2 # variable overriding
+num2 = 9 # variable overriding
+output = num2 + num1 # addition
+output = num2 - num1 # sub-traction
+output = num2 / num1 # division
+output = num2 // num1 # floor division
+output = num2 % num1 # modulus => returns the remainder
+output = num2 * num1 # multiplication
+output = num2 ** num1 # multiplication
+
+print("==================================================")
+print(output)
+print("==================================================")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
